f10eae8fb836aa643f64a2f4ccdb0b243d27e71f
akash patel <avpatel.98@gmail.com>
2022-07-21 12:11:23 +0530

    add  google authentication using oaut20h

--- app.js
+++ app.js
@@ -7,19 +7,18 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose")
-
-
+const findOrCreate = require("mongoose-findorcreate");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config()
+const dotenv=require('dotenv');
 const app = express();
-
-
-
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(session({
 
-    secret: "our little secret.",
+    secret: "our  secret.",
     resave: false,
     saveUninitialized: false
 
@@ -31,11 +30,14 @@ app.use(passport.session());
 
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true })
-// mongoose.set("useCreateIndex", true);
+
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 userSchema.plugin(passportLocalMongoose);
 
@@ -48,15 +50,49 @@ passport.use(user.createStrategy());
 passport.serializeUser(user.serializeUser());
 passport.deserializeUser(user.deserializeUser());
 
-app.get("/logout",function(req,res){
-    req.logout(()=>{
-        res.redirect("/"); 
-    })
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
   
+
+passport.use(new GoogleStrategy({
+    clientID: "170968576442-b87mi4gvd9v1mek3kg1d6acfgp42mmid.apps.googleusercontent.com",
+    clientSecret: "secret=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",
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+app.get("/logout", function (req, res) {
+    req.logout(() => {
+        res.redirect("/");
+    })
+
 })
 app.get("/", function (req, res) {
     res.render("home")
 })
+
+app.get("/auth/google",
+    passport.authenticate("google",{ scope: ['profile'] 
+})
+)
+
+app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 
 app.get("/login", function (req, res) {
     res.render("login")
@@ -66,13 +102,13 @@ app.get("/register", function (req, res) {
     res.render("register")
 })
 
-app.get("/secrets",function(req,res){
-    if(req.isAuthenticated()){
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
         res.render("secrets")
-    }else{
+    } else {
         res.redirect("/login")
     }
-    })
+})
 
 app.post("/register", function (req, res) {
 
@@ -82,7 +118,7 @@ app.post("/register", function (req, res) {
             res.redirect("/register");
         } else {
             passport.authenticate("local")(req, res, function () {
-                res.redirect("/secrets"); 0 
+                res.redirect("/secrets"); 0
             })
         }
     })
@@ -90,21 +126,21 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
 
-const uaer = new user({
-    username : req.body.username,
-    password: req.body.password
+    const uaer = new user({
+        username: req.body.username,
+        password: req.body.password
 
-})
-req.login(user,function(err){
-if(err){
-    console.log(err);
-
-}else{
-    passport.authenticate("local")(req,res,function(){
-        res.redirect("/secrets");
     })
-}
-})
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            })
+        }
+    })
 })
 app.listen("3000", function (req, res) {
     console.log("server is running on 3000");
